6bd1207cf736b59c38f8dea75f71e2e0e97bf65f
Dongwan Ryu <themunyang21@naver.com>
2013-01-02 12:20:01 +0900

    replaced str_replace() with ltrim()

--- bmsparser_class.php
+++ bmsparser_class.php
@@ -1,15 +1,15 @@
 <?php
 // A Be-Music Source(BMS) File Parser for PHP by nandarous (themunyang21 at nate dot com)
-// Version 0.1 (2012.2.9). Last Changed: 2013.1.1
+// Version 0.2 (2013.1.2). Last Changed: 2013.1.2
 // This code is licensed under GNU Lesser General Public License (GNU LGPL) or a BSD-style licenses.
 // for texts of the license, please see http://www.gnu.org/licenses/lgpl.html
-// This code requires that PHP Version 5 is configured on your host webserver. (in other words, your webhosting provider must support PHP Version 5)
+// This code requires that Pyour webhosting provider must support PHP Version 5.
 // if the host server is what you managing now, please see http://php.net/manual/en/install.php
 // this code supports all variant of Be-Music Source File formats: BMS (the standard), BME (for 7/14 keys like Beatmania IIDX), BML (for 'long-notes' like Ez2DJ), PMS (for Pop'n Music like games).
 // for the original format specification of BMS files, see http://bm98.yaneu.com/bm98/bmsformat.html
 
 class BMS_Parser{
- const BR_VERSION="0.1.9.9";
+ const BP_VERSION="0.2.0.0a";
 
  // Directives for basic information (metadatas)
  const B_PLAYTYPE="PLAYER"; // Play mode
@@ -31,7 +31,7 @@ class BMS_Parser{
  const DP9KEYS=25; // Double Play (9 keys) / 11~15: Keys 1~5, 22~25: Keys 6~9
  const DP10KEYS=26; // Double Play (10 keys) / 11~15: 1P Keys 1~5, 16: 1P SC, 21~25: 2P Keys 1~5, 26: 2P SC
  const DP14KEYS=29; // Double Play (14 keys) / 11~15: 1P Keys 1~5, 16: 1P SC, 18~19: 1P Keys 6~7, 21~25: 2P Keys 1~5, 26: 2P SC, 28~29: 2P Keys 6~7
- // for Long-notes
+ // for Long-notes (standard notes + 40)
  const LN_SP5KEYS=56;
  const LN_SP7KEYS=59;
  const LN_DP9KEYS=65;
@@ -60,7 +60,7 @@ class BMS_Parser{
   $flagsfound=0;
   $playtype_def=false;
 
-  $data["BMS_Reader_Version"]=self::BR_VERSION;
+  $data["BMS_Parser_Version"]=self::BP_VERSION;
   $data["filepath"]=str_replace(DIRECTORY_SEPARATOR,"/",realpath($this->path));
   $data["path"]=dirname($data["filepath"]);
   $data["filename"]=basename($this->path);
@@ -71,7 +71,8 @@ class BMS_Parser{
   rewind($this->handle);
   while(($parsing=fgets($this->handle)) !== false){
    $parameter=strstr($parsing," ",true);
-   $parameter=str_replace("#","",$parameter);
+   //$parameter=str_replace("#","",$parameter);
+   $parameter=ltrim($parameter,"#");
    $parameter=strtoupper($parameter);
    
    $value=strstr($parsing," ");
@@ -164,7 +165,7 @@ class BMS_Parser{
   rewind($this->handle);
   while(($parsing=fgets($this->handle)) !== false && !feof($this->handle)){
    $param_id=strstr($parsing,":",true);
-   $param_id=str_replace("#","",$param_id);
+   $param_id=ltrim($param_id,"#");
    if(preg_match("/^([0-9]{5})$/",$param_id)){
     $track=substr($param_id,0,3);
     $channel=substr($param_id,3,2);
@@ -199,7 +200,7 @@ class BMS_Parser{
   rewind($this->handle);
   $buffers=array();
   while(($lines=fgets($this->handle)) !== false){
-   $ch_params=str_replace("#","",strstr($lines,":",true));
+   $ch_params=ltrim(strstr($lines,":",true),"#");
    if(preg_match("/^([0-9]{5})$/",$ch_params)){
     $channel_id=substr($ch_params,3,2);
     $standardnotes=($channel_id >= 11 && $channel_id < 30);
@@ -240,7 +241,7 @@ class BMS_Parser{
   $data["number_keysounds"]=0;
   $data["number_images"]=0;
   while(($parsing=fgets($this->handle)) !== false){
-   $param=str_replace("#","",strstr($parsing," ",true));
+   $param=ltrim(strstr($parsing," ",true),"#");
    if(preg_match("/^(WAV)([A-Za-z0-9]{2})$/i",$param)){
     $soundid=substr($param,3,2);
     $soundname=trim(strstr($parsing," "));
